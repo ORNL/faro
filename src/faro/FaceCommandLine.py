3f933661c2625db342f337c8162c08f815cbed29
David Bolme <bolmeds@ornl.gov>
2020-06-24 12:08:44 -0400

    Added a status command to the client.  Fixed missing subject_id for enrollment.

--- src/faro/FaceCommandLine.py
+++ src/faro/FaceCommandLine.py
@@ -259,6 +259,81 @@ def detectParseOptions():
 
     return options, args
 
+def statusParseOptions():
+    """
+    Parse command line arguments.
+    """
+    args = ['']  # Add the names of arguments here.
+    n_args = len(args)
+    args = " ".join(args)
+    description = '''Check the server status and display algorithm and version information.'''
+    epilog = '''Created by David Bolme - bolmeds@ornl.gov'''
+
+    version = faro.__version__
+
+    # Setup the parser
+    parser = optparse.OptionParser(usage='%s command [OPTIONS] %s' % (sys.argv[0], args),
+                                   version=version, description=description, epilog=epilog)
+
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
+                      help="Print out more program information.")
+
+    addConnectionOptions(parser)
+    # Here are some templates for standard option formats.
+    # parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=True,
+    #                 help="Decrease the verbosity of the program")
+
+    # parser.add_option("-b", "--bool", action="store_true", dest="my_bool", default=False,
+    #                  help="don't print status messages to stdout")
+
+    # parser.add_option( "-c","--choice", type="choice", choices=['c1','c2','c3'], dest="my_choice", default="c1",
+    #                  help="Choose an option.")
+
+    # parser.add_option( "-f","--float", type="float", dest="my_float", default=0.0,
+    #                  help="A floating point value.")
+
+    # parser.add_option( "-i","--int", type="int", dest="my_int", default=0,
+    #                  help="An integer value.")
+
+    # parser.add_option( "-s","--str", type="str", dest="my_str", default="default",
+    #                  help="A string value.")
+
+    # parser.add_option( "--enroll", type="str", dest="enroll_gallery", default=None,
+    #                  help="Enroll detected faces into a gallery.")
+
+    # parser.add_option( "--search", type="str", dest="search_gallery", default=None,
+    #                  help="Search images for faces from a gallery.")
+
+    # parser.add_option( "--name", type="str", dest="subject_name", default=None,
+    #                  help="Enroll detected faces into a gallery.")
+
+    # parser.add_option( "--subject-id", type="str", dest="subject_id", default=None,
+    #                  help="Enroll detected faces into a gallery.")
+
+    # parser.add_option( "--search-log", type="str", dest="search_log", default=None,
+    #                  help="Enroll detected faces into a gallery.")
+
+    # parser.add_option( "-m","--match-log", type="str", dest="match_log", default=None,
+    #                  help="A directory to store matching faces.")
+
+    # parser.add_option( "--same-person", type="str", dest="same_person", default=None,
+    #                  help="Specifies a python function that returns true if the filenames indicate a match.  Example: lambda x,y: x[:5] == y[:5]")
+
+    # parser.add_option( "-s","--scores-csv", type="str", dest="scores_csv", default=None,
+    #                  help="Save similarity scores to this file.")
+
+    # Parse the arguments and return the results.
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.print_help()
+        print()
+        print(("Please supply exactly %d arguments." % 0))
+        print()
+        exit(-1)
+
+    return options, args
+
 
 def enrollParseOptions():
     """
@@ -1186,7 +1261,7 @@ def enroll():
         results = face_client.detectExtractEnroll(im, enroll_gallery=options.enroll_gallery, best=options.best,
                                                   threshold=options.detect_thresh, min_size=options.min_size,
                                                   run_async=True, source=filename, frame=-1,
-                                                  subject_name=options.subject_name)
+                                                  subject_name=options.subject_name, subject_id=options.subject_id)
 
         detect_queue.append([im, results, options])
         enroll_queue.append([im, results, options])
@@ -1342,8 +1417,24 @@ def test():
     if len(video_list) > 0:
         print("WARNING: Video Processing Is Not Implemented. %d videos skipped." % (video_list,))
 
+def status():
+    """
+    Conects to the server and gets status information.
+    """
+    options,args = statusParseOptions()
+
+    face_client = connectToFaroClient(options)
+
+    message = face_client.status()
+
+    print()
+    print (message)
+    print()
+
+ 
 
 COMMANDS = {
+    'status' : ['Connects to the server and displays version and status information.',status],
     'detect' : ['Only run face detection.',detect],
     'detectExtract' : ['Run face detection and template extraction.',detectExtract],
     'extractOnly' : ['Only run face extraction and attribute extraction.',extractOnly],
