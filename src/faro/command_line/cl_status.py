00a5ffaf003c8d343d06162ce5a930f3aa57204f
Joel <broganjr@ornl.gov>
2021-07-13 18:33:25 -0400

    added update_service function to status servicelistener

--- src/faro/command_line/cl_status.py
+++ src/faro/command_line/cl_status.py
@@ -81,7 +81,7 @@ class ServiceListener:
         # print("Service %s removed" % (name,))
         del self.availableServices[name]
         del self.availableServices_tableform[name]
-    def update(self):
+    def update_service(self):
         pass
     def add_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
