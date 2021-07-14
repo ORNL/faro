f681035fe3cd538aa5c2c4df4f56899ec3f99bb4
Joel <broganjr@ornl.gov>
2021-07-14 13:20:49 -0400

    added name to default options

--- src/faro/FaceClient.py
+++ src/faro/FaceClient.py
@@ -60,7 +60,7 @@ def getDefaultClientOptions():
     options.verbose = False
     options.quality=95
     options.compression='uint8'
-    options.service_name ='default'
+    options.service_name =None
     return options
 
 
